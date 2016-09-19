29ae182604365106317c83d9d66ecdf8f017bea4
Kjasparov <kasperrostvold@Kaspers-MacBook-Pro.local>
2016-09-19 14:09:07 +0200

    hello

--- server_http.hpp
+++ server_http.hpp
@@ -357,6 +357,7 @@ namespace SimpleWeb {
                     if(!ec) {
                         if(timeout_content>0)
                             timer->cancel();
+                        /*
                         auto http_version=stof(request->http_version);
                         
                         auto range=request->header.equal_range("Connection");
@@ -366,7 +367,25 @@ namespace SimpleWeb {
                         }
                         if(http_version>1.05)
                             read_request_and_content(response->socket);
+                            */
+                            
+                    try {
+                                auto http_version=stof(request->http_version);
+                                auto range=request->header.equal_range("Connection");
+                                for(auto it=range.first;it!=range.second;it++) {
+                                    if(boost::iequals(it->second, "close"))
+                                        return;
+                                }
+                                if(http_version>1.05)
+                                    read_request_and_content(response->socket);
+                                
+                            } catch (const std::exception &e){
+                                if(exception_handler) 
+                                exception_handler(e); 
+                                return;
+                            }        
                     }
+                    
                 });
             });
 
